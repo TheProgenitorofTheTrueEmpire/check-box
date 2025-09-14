d11ec92590f0624bb8c0ce4735e9c22185fd9208
TheProgenitorofTheTrueEmpire <the.progenitor.of.the.true.empire@gmail.com>
2025-09-14 20:49:28 +0330

    Add files via upload

---
+++ scripet.js
@@ -0,0 +1,14 @@
+// let txt = "Amir reza";
+// let fullnameArray =txt.split(" ");
+// for (let index = 0; index < fullnameArray.length; index++){
+//     console.log(fullnameArray[index]);
+// }
+let Arrayname =["Abas" , "Zare" ,  "0000888000"  , 1260];
+let txt ="<tr>";
+for (let index = 0; index < Arrayname.length; index++){
+let information = Arrayname[index];
+"</tr>" 
+   
+}
+document.getElementById("table").innerHTML += Arrayname
+// document.getElementById("demo").innerHTML(Arrayname);
